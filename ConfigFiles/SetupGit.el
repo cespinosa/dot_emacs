14dd56a13bc9b940c5cfb1922bb15f0be5a5884c
cespinosa <cespinosa@astro.unam.mx>
2021-05-10 19:47:46 -0500

    Add utils

--- ConfigFiles/SetupGit.el
+++ ConfigFiles/SetupGit.el
@@ -2,6 +2,18 @@
 ;; magit: the git porcelain to manage git
 
 (use-package magit
+  :config
+
+  ;; Refresh `magit-status' after saving a buffer
+  (add-hook 'after-save-hook #'magit-after-save-refresh-status)
+  )
+
+(use-package git-timemachine
+  )
+
+(use-package diff-hl
+  :init
+  (global-diff-hl-mode)
   )
 
 (provide 'SetupGit)
