ca8480f9437c9a1599faec01fd02f6ea6c72fdc4
cespinosa <cespinosa@astro.unam.mx>
2021-05-28 12:09:10 -0500

    Change font

--- ConfigFiles/SetupFont.el
+++ ConfigFiles/SetupFont.el
@@ -3,15 +3,15 @@
   "Configure faces on frame creation"
   (select-frame frame)
   (if (display-graphic-p)
-      (when (member "FiraCode Nerd Font" (font-family-list))
-        (set-frame-font "FiraCode Nerd Font 12" nil t))
+      (when (member "Hasklug Nerd Font" (font-family-list))
+        (set-frame-font "Hasklug Nerd Font 12" nil t))
       )
   )
 
 (add-hook 'after-make-frame-functions #'rag-set-face)
 
 ;; set frame font when running emacs normally
-(when (member "FiraCode Nerd Font" (font-family-list))
-  (set-frame-font "FiraCode Nerd Font 12" nil t))
+(when (member "Hasklug Nerd Font" (font-family-list))
+  (set-frame-font "Hasklug Nerd Font 12" nil t))
 
 (provide 'SetupFont)
